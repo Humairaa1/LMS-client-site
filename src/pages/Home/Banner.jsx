63a3464370af8bc09fadd1819fea24f185dd256c
Humaira <humaira71bd@gmail.com>
2025-06-25 11:56:03 -0700

    banner

---
+++ src/pages/Home/Banner.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import slide1 from '../../assets/slide-1.jpg'
+import slide2 from '../../assets/slide2.jpeg'
+import slide3 from '../../assets/slide3.jpeg'
+import slide4 from '../../assets/slide4.jpg'
+
+export default function Banner() {
+    return (
+        <div className="carousel w-full h-[200px] md:h-[300px] lg:h-[500px]">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img
+                    src={slide1}
+                    className="w-full" />
+                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                    <a href="#slide4" className="btn btn-circle">❮</a>
+                    <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+                <img
+                    src={slide2}
+                    className="w-full" />
+                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                    <a href="#slide1" className="btn btn-circle">❮</a>
+                    <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+                <img
+                    src={slide3}
+                    className="w-full" />
+                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                    <a href="#slide2" className="btn btn-circle">❮</a>
+                    <a href="#slide4" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+                <img
+                    src={slide4}
+                    className="w-full" />
+                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                    <a href="#slide3" className="btn btn-circle">❮</a>
+                    <a href="#slide1" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Banner from './Banner'
 
 export default function Home() {
   return (
     <div>
-      this is home page
+      <Banner></Banner>
     </div>
   )
 }
